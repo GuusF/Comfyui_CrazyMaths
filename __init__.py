b827b8d8a87c7e54d2b095800b9f759739affa4b
GuusF <70903780+GuusF@users.noreply.github.com>
2025-08-14 04:44:11 -0700

    Add files via upload
    Added the first version that I developed locally. still a lot of bugs to fix and features to implement!

---
+++ __init__.py
@@ -0,0 +1,34 @@
+from .math_alpha_mask import (
+    MathAlphaMask,
+    FractalNoiseAlphaMask,
+    VoronoiAlphaMask,
+    SierpinskiAlphaMask,
+    HarmonographAlphaMask,
+    AttractorAlphaMask,
+    QuasicrystalAlphaMask,
+    EquationAlphaMask
+)
+
+NODE_CLASS_MAPPINGS = {
+    "MathAlphaMask": MathAlphaMask,
+    "FractalNoiseAlphaMask": FractalNoiseAlphaMask,
+    "VoronoiAlphaMask": VoronoiAlphaMask,
+    "SierpinskiAlphaMask": SierpinskiAlphaMask,
+    "HarmonographAlphaMask": HarmonographAlphaMask,
+    "AttractorAlphaMask": AttractorAlphaMask,
+    "QuasicrystalAlphaMask": QuasicrystalAlphaMask,
+    "EquationAlphaMask": EquationAlphaMask,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "MathAlphaMask": "Crazy Math - Waveform Mask 🤯	",
+    "FractalNoiseAlphaMask": "Crazy Math - Fractal Noise 🤯",
+    "VoronoiAlphaMask": "Crazy Math - Voronoi 🤯",
+    "SierpinskiAlphaMask": "Crazy Math - Sierpinski 🤯",
+    "HarmonographAlphaMask": "Crazy Math - Harmonograph 🤯",
+    "AttractorAlphaMask": "Crazy Math - Strange Attractor 🤯",
+    "QuasicrystalAlphaMask": "Crazy Math - Quasicrystal 🤯",
+    "EquationAlphaMask": "Crazy Math - Equation 🤯",
+}
+
+
